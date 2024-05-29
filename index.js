f20085efd20fad8f863303ccf3be2bec9597847b
mahmudul-hasan01 <mahamudul2018@gmail.com>
2024-05-29 20:33:44 +0600

    connect with mongodb

--- index.js
+++ index.js
@@ -1,7 +1,45 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express')
 const app = express()
+const cors = require('cors')
+require('dotenv').config()
 const port =process.env.PORT || 5000
 
+app.use(cors({
+   origin:[
+    'http://localhost:5173'
+   ] ,
+   credentials: true,
+}))
+app.use(express.json())
+
+// mongodb
+
+const uri = `mongodb+srv://${process.env.USER_ID}:${process.env.USER_PASS}@cluster0.uoehazd.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+   
+    
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+// express
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
